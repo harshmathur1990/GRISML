4adf4ce1727ca5318a94cf7ee61f168f0383cc69
Harsh Mathur <harshmathur.1990@gmail.com>
2026-02-16 06:36:04 +0100

    added multi gpu support

--- train.py
+++ train.py
@@ -1,4 +1,8 @@
 import os
+os.environ["NCCL_P2P_DISABLE"] = "1"
+os.environ["NCCL_IB_DISABLE"] = "1"
+os.environ["NCCL_SHM_DISABLE"] = "0"
+os.environ["NCCL_DEBUG"] = "WARN"
 import h5py
 from astropy.io import fits
 import torch
