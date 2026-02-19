fae042711012b5237516d13eac04ed2c11d9d58c
Harsh Mathur <harshmathur.1990@gmail.com>
2026-02-19 15:51:41 +0100

    something

--- train.py
+++ train.py
@@ -3,9 +3,14 @@ os.environ["NCCL_P2P_DISABLE"] = "1"
 os.environ["NCCL_IB_DISABLE"] = "1"
 os.environ["NCCL_SHM_DISABLE"] = "0"
 os.environ["NCCL_DEBUG"] = "INFO"
+os.environ["PYTORCH_NVML_BASED_CUDA_CHECK"] = "0"
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"   # optional, helps stability
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 import h5py
 from astropy.io import fits
 import torch
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
